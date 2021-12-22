9dbb4c6a7fa873bef618476eff3e64f0b8fbe536
daryoush12 <farsimadan.daryoush@gmail.com>
2021-12-23 00:04:30 +0200

    async the functions

--- src/controllers/story.ts
+++ src/controllers/story.ts
@@ -14,36 +14,37 @@ const router = Router();
  * @param req 
  * @param res 
  */
-export const createStory = async(req: Request, res: Response, ): Promise<void> => {
+export async function createStory(req: Request, res: Response): Promise<void> {
     const storyParam: StoryDocument = req.body;
     if(!storyParam) res.send("Bad param");
 
     const newStory = new Story(storyParam);
     await newStory.save();
     res.send(CREATION_SUCCESS);
-};
+}
 
-export function health (req: Request, res: Response): void {
+export async function health (req: Request, res: Response): Promise<void> {
 
     res.status(200).send("Wonderful health");
 }
 
-export function editStory (req: Request, res: Response): void {
+export async function editStory (req: Request, res: Response): Promise<void> {
     const story:StoryDocument = req.body;
     const {documentId} = req.params;
 
     if(!story) res.send(MODIFY_FAIL);
     if(!documentId) res.send(ID_FAIL);
     
-    Story.updateOne({id: documentId}, story);
+    await Story.updateOne({id: documentId}, story);
+    res.send("Edit success");
 }
 
-export function deleteStory (req: Request, res:Response):void {
+export async function deleteStory (req: Request, res:Response):Promise<void> {
     const documentId:string = req.params?.id;
 
     if(!documentId) if(!documentId) res.send(ID_FAIL);
 
-    Story.deleteOne({id:documentId});
+    await Story.deleteOne({id:documentId});
     res.status(301).send("Resource deleted");
 }
 
